ba9c281e8284449e7f5435e8bfdcba470fb9c358
wswind <18399096+wswind@users.noreply.github.com>
2019-10-31 14:35:29 +0800

    up

---
+++ ShaConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ShaConsole
+{
+    public static class SHAExtensions
+    {
+        public static string Sha256(this string input)
+        {
+            using (SHA256 sHA = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(input);
+                return Convert.ToBase64String(sHA.ComputeHash(bytes));
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string password = "secret";
+            Console.WriteLine(password.Sha256());
+        }
+    }
+}
